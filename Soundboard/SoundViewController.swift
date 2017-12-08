7ad91ca5ece65f25a9e0ed23fa87cc36ae524552
Sophie Amin <aminsophie@gmail.com>
2017-12-08 16:37:12 -0700

    Added elements to the storyboard and cocoa touch class

---
+++ Soundboard/SoundViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SoundViewController.swift
+//  Soundboard
+//
+//  Created by Sophie Amin on 2017-12-08.
+//  Copyright © 2017 Sophie Amin. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var soundNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
